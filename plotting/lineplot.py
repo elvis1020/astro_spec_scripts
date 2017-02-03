0ca1f8cc78e6e0e0c45b8dab5faf2e688f4543fe
elvis1020 <elvis.shock@gmail.com>
2017-02-03 21:15:05 +0000

    lineplot completely rewritten

--- plotting/lineplot.py
+++ plotting/lineplot.py
@@ -3,219 +3,270 @@
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.ticker import MultipleLocator, FormatStrFormatter
-import matplotlib.gridspec as gridspec
-from matplotlib import rc
-import scipy.optimize as opt
-import scipy.interpolate as itp
+import pyfits
+
 
 def bissec(A,x):
-	n = len(A)-1
-	if (x < A[0]):
-		return 0
-	elif (x > A[n]):
-		return n+1
-	n1 = 0
-	n2 = n
-	while ((n2-n1)>1):
-		nm = (n1+n2)/2
-		if ((x-A[nm]) > 0):
-			n1 = nm
-		else:
-			n2 = nm
-	return n1
+    
+    n = len(A)-1
+    if (x < A[0]):
+        return 0
+    elif (x > A[n]):
+        return n+1
+    n1 = 0
+    n2 = n
+    while ((n2-n1)>1):
+        nm = (n1+n2)/2
+        if ((x-A[nm]) > 0):
+            n1 = nm
+        else:
+            n2 = nm
+    
+    return n1
 
-def is_number(s):
-    try:
-        float(s)
-        return True
-    except ValueError:
-        return False
-        
+
 def readfits(file):
-	import pyfits
-	spectra = pyfits.open(file)
-	try:
-		bin = spectra[0].header['CDELT1']
-	except:
-		bin = spectra[0].header['CD1_1']
-		print bin
-	lambda0 = spectra[0].header['CRVAL1']
-	flux = spectra[0].data
-	x=[lambda0+i*bin for i in range(len(flux))]
-	y=[i for i in flux]
-	return x,y
+    
+    spectra = pyfits.open(file)
+    try:
+        bin = spectra[0].header['CDELT1']
+    except:
+        bin = spectra[0].header['CD1_1']
+        print bin
+    lambda0 = spectra[0].header['CRVAL1']
+    flux = spectra[0].data
+    x = np.array([lambda0 + i * bin for i in range(len(flux))])
+    y = np.array([i for i in flux])
+    
+    return [x, y]
 
-def adjspec(x, flcor, lbcor):
-	modf = itp.interp1d(wlmod[line], fluxmod[line])
-	return modf(x-lbcor)*flcor
+
+def rec_pars(line_id, cols):
+    
+    line_data[line_id]['lambda'] = float(line_id.split()[1])
+    line_data[line_id]['range'] = float(cols[6])
+    line_data[line_id]['obpars']['wlcorr'] = float(cols[4])
+    line_data[line_id]['obpars']['flxcorr'] = float(cols[5])
+    line_data[line_id]['obpars']['style'] = cols[7]
+    line_data[line_id]['stpars']['map'] = cols[8]
+    line_data[line_id]['stpars']['width'] = float(cols[9])
+    line_data[line_id]['stpars']['alpha'] = float(cols[10])
+
+
+def rec_spec(line_id, cols):
+    
+    if cols[0] not in file_library.keys():
+        file_library[cols[0]] = {'type': 'spectrum', 'status': 'closed'}
+    line_data[line_id]['obspec'] = file_library[cols[0]]
+
+    if (cols[1][0] != '@') and (cols[1] != 'none'):
+        if cols[1] not in file_library.keys():
+            file_library[cols[1]] = {'type': 'spectrum', 'status': 'closed'}
+        line_data[line_id]['stspec'] = {cols[1]: file_library[cols[1]]}
+    elif cols[1] == 'none':
+        line_data[line_id]['stspec'] = 'none'
+    else:
+        line_data[line_id]['stspec'] = {}
+        lst = open(cols[1][1:])
+        for coll in (raw.strip().split() for raw in lst):
+            if coll[0] not in file_library.keys():
+                file_library[coll[0]] = {'type': 'spectrum', 'status': 'closed'}
+            line_data[line_id]['stspec'][coll[0]] = file_library[coll[0]]
+
+
+def build_data(inptfil):
+    
+    global line_data
+    global par_rec
+    line_data = {}
+    par_rec = []
+    with open(inptfil) as l:
+        ln = l.readlines()[1:]
+    for cols in (raw.strip().split() for raw in ln):
+        line_id = cols[2]+' '+cols[3]
+        if cols[0][0] != '#':
+            par_rec.append(cols)
+            line_data[line_id] = {'obpars': {}, 'stpars': {}}
+            rec_spec(line_id, cols)
+            rec_pars(line_id, cols)
+    line_data.keys().sort()
+    
+    return line_data
+
+
+def reload_data(inptfil):
+    
+    changed_lines = []
+    with open(inptfil) as l:
+        ln = l.readlines()[1:]
+    for cols in (raw.strip().split() for raw in ln):
+        line_id = cols[2]+' '+cols[3]
+        if cols[0][0] != '#' and line_id in line_data.keys() and cols not in par_rec:
+            rec_spec(line_id, cols)    
+            rec_pars(line_id, cols)
+            changed_lines.append(line_id)
+            for n, i in enumerate(par_rec):
+                if i[3] == cols[3]:
+                    par_rec[n] = cols
+
+    return changed_lines
+
+
+def reload_pars(inptfil):
+    
+    changed_lines = []
+    with open(inptfil) as l:
+        ln = l.readlines()[1:]
+    for cols in (raw.strip().split() for raw in ln):
+        line_id = cols[2]+' '+cols[3]
+        if cols[0][0] != '#' and line_id in line_data.keys() and cols not in par_rec:
+            rec_pars(line_id, cols)
+            changed_lines.append(line_id)
+            for n, i in enumerate(par_rec):
+                if i[3] == cols[3]:
+                    par_rec[n] = cols
+
+    return changed_lines
+
+
+def update_library(file_library):
+    
+    for entry in file_library.keys():
+        if file_library[entry]['status'] == 'closed':
+            if file_library[entry]['type'] == 'spectrum':
+                if entry[-5:] == '.fits':
+                    file_library[entry]['type'] = 'fits_spectrum'
+                else:
+                    file_library[entry]['type'] = 'ascii_spectrum'
+                    
+            if file_library[entry]['type'] == 'fits_spectrum':
+                try:
+                    file_library[entry]['data'] = {}
+                    spec = readfits(entry)
+                    file_library[entry]['data']['x'] = spec[0]
+                    file_library[entry]['data']['y'] = spec[1]
+                    file_library[entry]['status'] = 'open'
+                except:
+                    print('file '+entry+' not found. skipping...\n')
+                    pass
+            
+            if file_library[entry]['type'] == 'ascii_spectrum':
+                try:
+                    file_library[entry]['data'] = {}
+                    spec = np.genfromtxt(entry, unpack=True)
+                    file_library[entry]['data']['x'] = spec[0]
+                    file_library[entry]['data']['y'] = spec[1]
+                    file_library[entry]['status'] = 'open'
+                except:
+                    print('file '+entry+' not found. skipping...\n')
+                    pass
+        
+        file_library.keys().sort()
+
+
+def slicer(line, spec, ctr, rge):
+    
+    obsx = spec['x']
+    x_idx = bissec(obsx, ctr)
+    x_m = bissec(obsx, ctr - rge * 0.7)
+    x_p = bissec(obsx, ctr + rge * 0.7)
+    xslice = spec['x'][x_m:x_p]
+    yslice = spec['y'][x_m:x_p]
+    
+    return [xslice, yslice]
+
+
+def draw_subplot(line, axn):
+    line_data[line]['subplot'] = {'axis': axn}
+    ctr = line_data[line]['lambda']
+    rge = line_data[line]['range']
+    
+    obslice = slicer(line, line_data[line]['obspec']['data'], ctr, rge)
+    line_data[line]['subplot']['obsplot'] = axn.plot(obslice[0] + line_data[line]['obpars']['wlcorr'],
+                                                     obslice[1] / line_data[line]['obpars']['flxcorr'],
+                                                     line_data[line]['obpars']['style'])[0]
+    
+    nplots = len(line_data[line]['stspec'].keys())
+    colors = plt.get_cmap(line_data[line]['stpars']['map'])(np.linspace(0, 1.0, nplots+2))
+    line_data[line]['subplot']['stplot'] = {}
+    for n, static in enumerate(line_data[line]['stspec'].keys()):
+        stslice = slicer(line, line_data[line]['stspec'][static]['data'], ctr, rge)
+        line_data[line]['subplot']['stplot'][static] = axn.plot(stslice[0], stslice[1], ls='-',
+                                                                 lw=line_data[line]['stpars']['width'],
+                                                                 alpha=line_data[line]['stpars']['alpha'],
+                                                                 color=colors[n+1])[0]
+    
+    ymin = 999999.
+    ymax = -999999.
+    for plot in axn.lines:
+        plty = plot.get_ydata()
+        ymn = min(plty)
+        ymx = max(plty)
+        if ymn < ymin:
+            ymin = ymn
+        if ymx > ymax:
+            ymax = ymx
+    
+    axn.set_xlim([ctr - rge / 2., ctr + rge / 2.])
+    yrange = ymax - ymin
+    axn.set_ylim([ymin - yrange*0.06, ymax + yrange*0.06])
+    line_data[line]['subplot']['text'] = axn.text(0.04, 0.06, line ,horizontalalignment='left',
+                                                  verticalalignment='bottom', fontsize=12,
+                                                  transform = plt.gca().transAxes)
+
+
+def draw_fig():
+    fig = plt.figure()
+    nrows = len(line_data.keys()) / 2 + 1
+    for n, line in enumerate(sorted(line_data.keys())):
+        axn = fig.add_subplot(nrows, 2, n+1)
+        draw_subplot(line, axn)
+        plt.gca().get_xaxis().get_major_formatter().set_useOffset(False)
+
+    #plt.xlabel('Wavelength [$\AA$]')
+    #plt.ylabel('Flux')
+    plt.tight_layout()
+    plt.ion()
+    plt.show()
+
+
+def redrawer(line):
+    line_data[line]['subplot']['obsplot'].remove()
+    for static in line_data[line]['subplot']['stplot'].keys():
+        line_data[line]['subplot']['stplot'][static].remove()
+    line_data[line]['subplot']['text'].remove()
+    draw_subplot(line, line_data[line]['subplot']['axis'])
 
 
 if len(sys.argv) > 1:
-	inptfil = sys.argv[1]
+    inptfil = sys.argv[1]
 else:
-	inptfil = 'line_input.dat'
+    inptfil = 'line_input.dat'
+file_library = {}
+line_data = build_data(inptfil)
+update_library(file_library)
+draw_fig()
 
-rc('font', family = 'serif', serif = 'cmr10')
-rc('xtick', labelsize=14) 
-rc('ytick', labelsize=14) 
-command = 'x'
-while command != 'q':
-	lines=inptfil
-	wlobs=[]
-	fluxobs=[]
-	wlmod=[]
-	fluxmod=[]
-	lnlst=[]
-	elm=[]
-	dspl=[]
-	norm=[]
-	ran=[]
-	clr=[]
-	stl=[]
-	count=0
-	multimod=[]
-	flagfit = False
-	with open(lines) as l:
-		ln = l.readlines()[1:]
-	for cols in (raw.strip().split() for raw in ln):
-		if cols[0][0] != '#':
-			wlobs.append([])
-			fluxobs.append([])
-			wlmod.append([])
-			fluxmod.append([])
-			if cols[0][-5:] != '.fits':
-				obs = np.genfromtxt(cols[0], unpack=True)
-				wlobs[count] = obs[0]
-				fluxobs[count] = obs[1]
-			else:
-				wlobs[count],fluxobs[count]=readfits(cols[0])
-			if (cols[1][0] != '@') and (cols[1] != 'none'):
-				multimod.append(1)
-				if cols[1][-5:] != '.fits':
-					mod = np.genfromtxt(cols[1], unpack=True)
-					wlmod[count] = mod[0]
-					fluxmod[count] = mod[1]
-				else:
-					wlmod[count],fluxmod[count]=readfits(cols[1])
-				if command == 'fit':
-					flagfit = True
-			elif cols[1] == 'none':
-				multimod.append('x')
-				wlmod[count],fluxmod[count]=[],[]
-			else:
-				multimod.append(0)
-				lst = open(cols[1][1:])
-				for coll in (raw.strip().split() for raw in lst):
-					multimod[-1] += 1
-					wlmod[count].append([])
-					fluxmod[count].append([])
-					if coll[0][-5:] != '.fits':
-						mltspc = np.genfromtxt(coll[0], unpack=True)
-						wlmod[count][multimod[-1]-1] = mltspc[0] 
-						fluxmod[count][multimod[-1]-1] = mltspc[1]
-					else:
-						wlmod[count][multimod[-1]-1],fluxmod[count][multimod[-1]-1]=readfits(coll[0])
-			elm.append(cols[2])
-			lnlst.append(float(cols[3]))
-			dspl.append(float(cols[4]))
-			norm.append(float(cols[5]))
-			ran.append(float(cols[6]))
-			clr.append(cols[7])
-			stl.append(cols[8])
-			count += 1
-
-#________plotting start________#
-
-	fignum=0
-	fig=plt.figure(figsize=(17,(3.6*(round(count/2.))+1.12)))
-	for line in range(len(lnlst)):
-		o1=bissec(wlobs[line],lnlst[line]-ran[line]-0.5)
-		o2=bissec(wlobs[line],lnlst[line]+ran[line]+0.5)
-		if multimod[line] == 1:
-			m1=bissec(wlmod[line],lnlst[line]-ran[line]-0.5)
-			m2=bissec(wlmod[line],lnlst[line]+ran[line]+0.5)
-		else:
-			m1=[]
-			m2=[]
-			if multimod[line] != 'x':
-				for m in range(multimod[line]):
-					m1.append(bissec(wlmod[line][m],lnlst[line]-ran[line]-0.5))
-					m2.append(bissec(wlmod[line][m],lnlst[line]+ran[line]+0.5))
-			else:
-				m1.append(0)
-				m2.append(0)
-		oblinex = np.array(wlobs[line][o1:o2]) + dspl[line]
-		obliney = np.array(fluxobs[line][o1:o2]) / norm[line]
-		#oblinex=[lb+dspl[line] for lb in oblinex_raw]
-		#obliney=[fx/norm[line] for fx in obliney_raw]
-		if multimod[line] == 1:
-			modlinex=(wlmod[line][m1:m2])
-			modliney=(fluxmod[line][m1:m2])
-		else: 
-			modlinex=[]
-			modliney=[]
-			if multimod[line] != 'x':
-				for m in range(multimod[line]):
-					modlinex.append(wlmod[line][m][m1[m]:m2[m]])
-					modliney.append(fluxmod[line][m][m1[m]:m2[m]])
-			else:
-				modlinex.append(0)
-				modliney.append(0)
-		A=int(fignum/2.)*(0.88*(round(count/2.))**(-0.95))
-		B=(0.2*(round(count/2.))**(-0.61))
-		ysize=0.741*(round(count/2.))**(-1.02)
-		ypos=B+(1-B-ysize-A-0.017)
-		fig.add_axes([0.09+(fignum%2)*0.47, ypos, 0.40, ysize])
-		plt.ion()
-		
-		if flagfit:
-			initial_guess = (1., 0.)
-			popt, cov = opt.curve_fit(adjspec, oblinex, obliney, p0=initial_guess)
-			plt.text(0.98, 0.06,popt[0],horizontalalignment='right',verticalalignment='bottom', fontsize=10, transform = plt.gca (). transAxes)
-			plt.text(0.98, 0.16,popt[1],horizontalalignment='right',verticalalignment='bottom', fontsize=10, transform = plt.gca (). transAxes)
-			
-		plt.plot(oblinex,obliney,stl[line])
-		plt.plot([oblinex[0], oblinex[-1]], [1, 1], 'b--', linewidth=0.5)
-		if multimod[line] == 1:
-			plt.plot(modlinex,modliney,'r-')
-		else:
-			if multimod[line] != 'x':
-				colors = plt.get_cmap(clr[line])(np.linspace(0, 1.0, multimod[line]+2))
-				for m in range(multimod[line]):             
-					plt.plot(modlinex[m],modliney[m], color=colors[m+1])
-		if elm[line] != 'none':
-			if  elm[line][0] != '#':
-				linename=elm[line] + ' ' + str(lnlst[line])
-			else:
-				linename=elm[line][1:]
-			plt.text(0.04, 0.06,linename,horizontalalignment='left',verticalalignment='bottom', fontsize=16, transform = plt.gca (). transAxes)
-		if multimod[line] == 1:
-			modmin=min(modliney)
-			modmax=max(modliney)
-		else:
-			modmin=min(obliney)
-			modmax=max(obliney)
-			if multimod[line] != 'x':
-				for m in range(multimod[line]):
-					if min(modliney[m]) < modmin:
-						modmin = min(modliney[m])
-					if max(modliney[m]) > modmax:
-						modmax = max(modliney[m])
-		plt.axis([lnlst[line]-ran[line], lnlst[line]+ran[line], (min([min(obliney),modmin]))-(max([max(obliney),modmax]))*0.05, (max([max(obliney),modmax]))+(max([max(obliney),modmax]))*0.05])
-		plt.gca().get_xaxis().get_major_formatter().set_useOffset(False)
-		mjtickx=(float(round((ran[line]*2./5.)*10)))/10
-		if mjtickx == 0:
-			mjtickx=(float(round((ran[line]*2./4.)*100)))/100
-		mntickx=mjtickx/5
-		plt.gca().xaxis.set_major_locator(MultipleLocator(mjtickx))
-		plt.gca().xaxis.set_minor_locator(MultipleLocator(mntickx))
-		yrange=(max([max(obliney),modmax]))*1.05 - (min([min(obliney),modmin]))*0.95
-		plt.gca().yaxis.set_major_locator(MultipleLocator(yrange/4))
-		plt.gca().yaxis.set_major_formatter(FormatStrFormatter('%1.2f'))
-		fignum += 1
-	plt.figtext(0.5,0.02,"Wavelength (angstroms)",fontdict={'fontsize':18}, horizontalalignment='center')
-	plt.figtext(0.01,0.5,"Arbitrary Flux",fontdict={'fontsize':18},rotation=90, verticalalignment='center')
-	plt.show()
-	command=raw_input('enter to refresh; q to quit\n')
-	plt.close()
+option = ''
+while option != 'q':
+    option = raw_input('enter to refresh parameters\nr to reload line data\n'+
+    	                'b to rebuild line data (necessary if lambda is change'+
+    	                'd)\nq to quit\n')
+    if option == '':
+        changes = reload_pars(inptfil)
+        for line in changes:
+            redrawer(line)
+    elif option == 'r':
+        changes = reload_data(inptfil)
+        update_library(file_library)
+        for line in changes:
+            redrawer(line)
+    elif option == 'b':
+        plt.close()
+        line_data = build_data(inptfil)
+        update_library(file_library)
+        draw_fig()
+    elif option == 'q':
+        plt.close()
+        exit()
+    option = ''
